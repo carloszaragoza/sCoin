6ddb120d9cca77e85760f3b093c4cf8c61d19cf7
nakamotoSAMtoshi <samtoshi.nakamoto1@gmail.com>
2014-02-22 18:28:53 -0700

    gravity update

--- src/main.h
+++ src/main.h
@@ -62,6 +62,11 @@ static const int COINBASE_MATURITY = 100;
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 /** Maximum number of script-checking threads allowed */
 static const int MAX_SCRIPTCHECK_THREADS = 16;
+
+static const int DIFF_FILTER_THRESHOLD_TESTNET =  30000;
+static const int DIFF_FILTER_THRESHOLD         = 30000;
+
+
 #ifdef USE_UPNP
 static const int fHaveUPnP = true;
 #else
